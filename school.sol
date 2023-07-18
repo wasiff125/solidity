dbb62b4b037298467e15e9212f79278e3c739f9a
WASIFF ALI SHAH <137802448+wasiff125@users.noreply.github.com>
2023-07-18 11:11:14 +0500

    Update school.sol

--- school.sol
+++ school.sol
@@ -9,13 +9,13 @@ contract school{
         int class;
     }
 
-    Student[] public makWay;
+    Student[] public makWay;                             //dynamic array, like list data type is pyhton, but it does not support multiple data types for one array.
 
-    mapping(uint256 => string) public rollToName;
+    mapping(uint256 => string) public rollToName;        //mapping is like dictinary data type used in pyhton.
 
     function addStudent(uint256 _roll, string memory _name, int _class) public{
-        makWay.push(Student(_roll, _name, _class));
-        rollToName[_roll] = _name;
+        makWay.push(Student(_roll, _name, _class));     //adding new student in the array, using push function of solidity also used in other high level languages.
+        rollToName[_roll] = _name;                      //mapping the roll no with name, so we can find student name easily by only giving his roll no.
     }
 
 }
