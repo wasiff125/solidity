1223560865f0cf80a3ee09c4a62bf9d16be3e4e5
WASIFF ALI SHAH <137802448+wasiff125@users.noreply.github.com>
2023-07-24 12:37:55 +0500

    Update FundMe.sol
    updates contract, with send money, receive money and other functions

--- FundMe.sol
+++ FundMe.sol
@@ -20,12 +20,53 @@ contract FundMe{
 
     mapping (address funder => uint256 amountFunded) public addressToAmountFunded;
 
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
     function fund() public payable{
         require(getConversionRate(msg.value) >= miniumUSD);
 
         funders.push(msg.sender);
         //pushing all the address to funder array to keep the track
         addressToAmountFunded[msg.sender]= addressToAmountFunded[msg.sender] + msg.value;
+    }
+
+    function withdraw() public onlyOwner {
+        /*created for loop, initialed funderIndex variable of uint256 datatype, 
+        funder should always be greater than total addresses int the funders array,
+        so that the only condition when its true for loop is breaked,
+        for example our array has total five addresses, and fundersIndex value 
+        reaches 6 so the lopp will be breaked here*/
+        for(uint256 funderIndex=0; funderIndex < funders.length; funderIndex++){
+            /*we create a variable named funder having datatype address, which is equal to
+            funders array and its index in large brackerts, to grab the addresses by index,
+            one by one*/
+            address funder = funders[funderIndex] ;
+            /*after grabbing the address from array in variable funder, we give it to,
+            mapping addressToAmount and we maek that amount zero*/
+            addressToAmountFunded[funder] = 0;
+
+
+            //clearing the array 
+            funders = new address[](0);
+
+            // there are three ways to withdraw/send money in solidity
+
+            //1. transfer 
+            //payable(msg.sender).transfer(address(this).balance);
+            //2. send <returns bool>
+            //bool sendSuccess = payable(msg.sender).send(address(this).balance);
+            //require(sendSuccess, "send failed");
+            //3. call (recommended) <returns bool, data of datatype bytes>
+            (bool callSuccess,) = payable(msg.sender).call{value: (address(this).balance)}("");
+            require(callSuccess, "call failed");
+
+
+
+        }
     }
 
     function getPrice() public view returns(uint256){
@@ -48,5 +89,8 @@ contract FundMe{
     }
     
   
-
+    modifier onlyOwner(){
+        require(msg.sender == owner, "sender is not owner");
+        _;
+    }
 }
